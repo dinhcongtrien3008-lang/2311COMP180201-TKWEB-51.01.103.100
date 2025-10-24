e6ccbcf87b42d935062a7e41a71c0d2e152abae9
dinhcongtrien3008-lang <dinhcongtrien3008@gmail.com>
2025-10-24 11:49:33 +0700

    24/10

--- 5101103100/script.js
+++
@@ -1,8 +0,0 @@
-let cart = 0;
-function addToCart(button) {
-  const product = button.closest('.product');
-  const priceElement = product.querySelector('.price');
-  const price = parseInt(priceElement.innerText);
-  cart += price;
-  document.getElementById("total").innerText = cart.toLocaleString();
-}
